7a019cff4cc12f246a6546360437633b8c9930bf
Roman Ivanov <ivanovrvl@gmail.com>
2021-10-26 00:18:50 +0300

    improvement

--- db/long_task/functions/recover_worker_tasks.sql
+++ db/long_task/functions/recover_worker_tasks.sql
@@ -3,6 +3,10 @@
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-	RETURN;
+	-- this is a place to restart or complete failed worker`s tasks
+    UPDATE long_task.task
+    SET state_id='CF', error='Worker failed'
+    --SET state_id='AW', worker_id=NULL
+    WHERE worker_id=p_worker_id AND state_id='AE';
 END;
 $function$
--- db/long_task/triggers/task/task_notify_iud_tr.sql
+++ db/long_task/triggers/task/task_notify_iud_tr.sql
@@ -1,3 +1,3 @@
 ﻿DROP TRIGGER IF EXISTS task_notify_iud_tr ON long_task.task;
 
-CREATE TRIGGER task_notify_iud_tr AFTER INSERT OR DELETE OR UPDATE OF state_id, next_start, shed_period_id, shed_period_count, group_id ON long_task.task FOR EACH ROW EXECUTE FUNCTION table_notify_iud()
+CREATE TRIGGER task_notify_iud_tr AFTER INSERT OR DELETE OR UPDATE OF state_id, command, next_start, shed_period_id, shed_period_count, group_id ON long_task.task FOR EACH ROW EXECUTE FUNCTION table_notify_iud()
