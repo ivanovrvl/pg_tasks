f82b95c03f8cbd5aabd236c3c0c52424cb311482
Roman Ivanov <ivanovrvl@gmail.com>
2021-11-09 01:17:21 +0300

    improvement

--- db/long_task/triggers/worker/worker_changed_notify_tr.sql
+++ db/long_task/triggers/worker/worker_changed_notify_tr.sql
@@ -1,3 +1,3 @@
 ﻿DROP TRIGGER IF EXISTS worker_changed_notify_tr ON long_task.worker;
 
-CREATE TRIGGER worker_changed_notify_tr AFTER INSERT OR DELETE OR UPDATE OF active, stop ON long_task.worker FOR EACH ROW EXECUTE FUNCTION long_task.worker_changed_notify()
+CREATE TRIGGER worker_changed_notify_tr AFTER INSERT OR DELETE OR UPDATE OF active, stop ON long_task.worker FOR EACH ROW EXECUTE FUNCTION worker_changed_notify()
